9d4ef813135123a34523b07300faedf342b85c50
Justin Swift <jlswift2@gmail.com>
2022-02-06 13:15:07 -0600

    commit tree cfa6c3fb54ff7e5f8b22b9fcfab151b2a57fc5a8

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-function addingEventListener() {
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+
+//     function clickAlert() {
+//         alert('I was clicked!');
+//     }
+
+// input.addEventListener('click', clickAlert);
+// }
+
+function addingEventListener(){
     const input = document.getElementById('input');
 
-    function clickAlert() {
-        alert('I was clicked!');
-    }
-
-input.addEventListener('click', clickAlert);
-}
+    input.addEventListener('click', () => alert('I was clicked!'));}
